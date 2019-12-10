0f48884834b7b0aadd81965a33d9d7c2a46058c0
gleo08 <prince24111996@gmail.com>
2019-12-10 22:24:23 +0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, flash, url_for, session, redirect, request, logging
+from flask import Flask, render_template, flash, url_for, session, redirect, request, jsonify
 from flask_mysqldb import MySQL
-import csv
+import json
 import pandas as pd
 from functools import wraps
 
@@ -15,7 +15,7 @@ app.config['MYSQL_DB'] = 'examreg'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
-mysql.connection.autocommit(on='true')
+#mysql.connection.autocommit(on=True)
 
 @app.route('/')
 def index():
@@ -39,13 +39,13 @@ def login():
         cur = mysql.connection.cursor()
 
         # Get user by username
-        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        result = cur.execute("SELECT * FROM users WHERE user_name = %s", [username])
 
         if result > 0:
             # Get stored hash
             data = cur.fetchone()
-            password = data['password']
-            role = data['role']
+            password = data['pass']
+            role = data['role_id']
 
             # Compare
             if password_candidate == password:
@@ -54,7 +54,7 @@ def login():
                 session['username'] = username
 
                 flash('You are now logged in', 'success')
-                if role == 1:
+                if role == 0:
                     session['logged_in1'] = True
                     return redirect(url_for('student'))
                 else:
@@ -130,15 +130,81 @@ def student():
 def admin():
     return render_template('admin.html')
 
-@app.route('/upload', methods=['POST'] )
-def upload():
+@app.route('/upload1', methods=['POST'] )
+def upload1():
+    mysql.connection.autocommit(on=True)
     cur = mysql.connection.cursor()
     f = request.files['inputFile']
     r = pd.read_csv(f, sep=',')
     for it in r.iterrows():
-        cur.execute('INSERT INTO test(id, name, score1, score2) VALUES(%s, %s, %s, %s)', it[1])
+       cur.execute('INSERT INTO users(user_name, pass, name, role_id) VALUES(%s, %s, %s, %s)', it[1])
+
     cur.close()
-    return "success"
+    flash('Import Success', 'success')
+    return redirect(url_for('admin'))
+
+@app.route('/upload2', methods=['POST'])
+def upload2():
+    mysql.connection.autocommit(on=True)
+    cur = mysql.connection.cursor()
+    f = request.files['inputFile']
+    r = pd.read_csv(f, sep=',')
+    for it in r.iterows():
+        cur.execute('INSERT INTO subjects_students(student_id, subject_id, contest_id, is_approved) VALUES(%s, %s, %s, %s)', it[1])
+
+    cur.close()
+    flash('Import Succes', 'success')
+    return redirect(url_for('admin'))
+
+@app.route('/information', methods=['GET'])
+def information():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT users.id, users.name, users.user_name, s.name, s.code, ss.is_approved FROM users INNER JOIN students_subjects ss ON users.id = ss.student_id INNER JOIN subjects s ON s.id = ss.subject_id WHERE users.role_id = 2 ORDER BY users.id ASC')
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    result = []
+    for r in rv:
+        result.append(dict(zip(row_headers, r)))
+    return json.dump(result)
+
+@app.route('/makeContest', methods=['POST'])
+def makeContest():
+    mysql.connection.autocommit(on=True)
+    if request.method == 'POST':
+        nameContest = request.form['namecontest']
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO contest(name) VALUES (%s)', [nameContest])
+
+    cur.close()
+
+@app.route('/addTiming', methods=['POST'])
+def addTiming():
+    mysql.connection.autocommit(on=True)
+    if request.method == 'POST':
+        name = request.get_json('name')
+        begin_time = request.get_json('begin_time')
+        end_time = request.get_json('end_time')
+        room_name = request.get_json('room_name')
+        subject_name = request.get_json('subject_name')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO timing(name, begin_time, end_time) VALUES (%s, %s, %s)', [name, begin_time, end_time])
+    cur.execute('INSERT INTO timing_room(')
+
+    cur.close()
+
+@app.route('/getListOfRoom', methods=['GET'])
+def getListofRoom():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM room')
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    result = []
+    for r in rv:
+        result.append(dict(zip(row_headers, r)))
+    return json.dump(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
